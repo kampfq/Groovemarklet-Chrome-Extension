534cf1a3b8f8d57dd6a163c797e8cff5a9e6dbe8
Benjamin Issleib <ben@kampfq.eu>
2016-02-25 22:41:27 +0100

    fixed deezer and google play issues. removed the external javascript, now all scripts are included in the plugin

--- groove.js
+++ groove.js
@@ -1,3 +1,3 @@
 (function () {
-    document.body.appendChild(document.createElement("script")).src = "https://cdn.rawgit.com/kampfq/azeirah.github.io/4a62e7bffb62c24cf39d4187a1eab6f37e808597/groovemarklet2dev.js"
+    document.body.appendChild(document.createElement("script")).src = chrome.extension.getURL('groovemarklet.js');
 })()
